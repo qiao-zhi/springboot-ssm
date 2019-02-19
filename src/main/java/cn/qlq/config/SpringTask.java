c860a2c779b0e382626304c4354e67a93a3e86db
qiaozhi <qiao_liqiang@163.com>
2019-02-19 22:23:04 +0800

    整合SpringTask

---
+++ src/main/java/cn/qlq/config/SpringTask.java
@@ -0,0 +1,10 @@
+package cn.qlq.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration // 通过该注解来表明该类是一个Spring的配置，相当于一个xml文件
+@EnableScheduling
+public class SpringTask {
+
+}
---
+++ src/main/java/cn/qlq/task/FirstAnnotationJob.java
@@ -0,0 +1,19 @@
+package cn.qlq.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstAnnotationJob {
+	private static int count;
+
+	@Scheduled(fixedRate = 10000)
+	public void cron() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			System.err.println("InterruptedException " + e);
+		}
+		System.out.println("spring anno task execute times " + count++);
+	}
+}
