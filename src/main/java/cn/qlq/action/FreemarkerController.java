113fe3cf43dae727ba28c3ce65aade65c878ac0c
qiao_zhi <qiao_liqiang@163.com>
2019-02-19 10:47:08 +0800

    引入整合freemarker模板相关配置

---
+++ src/main/java/cn/qlq/action/FreemarkerController.java
@@ -0,0 +1,38 @@
+package cn.qlq.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 返回freemarker相关页面
+ * 
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("freemarker")
+public class FreemarkerController {
+
+	/**
+	 * 会跳转到templates/freemarker/index.ftl
+	 * 
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping("/index")
+	public String index(ModelMap map) {
+		map.addAttribute("name", "testname");
+		return "freemarker/index";
+	}
+
+	/**
+	 * 会跳转到templates/freemarker/center/center.ftl
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/center")
+	public String center() {
+		return "freemarker/center/center";
+	}
+}
---
+++ src/main/resources/templates/freemarker/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8" />
+    <title>FreeMarker模板引擎</title>
+</head>
+<body>
+FreeMarker模板引擎
+<h1>${name}</h1>
+</body>
+</html>
