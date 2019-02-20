c0d09724869680244cf3d4dc71a4cd7b45f49be2
qiaozhi <qiao_liqiang@163.com>
2019-02-20 22:46:50 +0800

    整合异步任务

--- src/main/java/cn/qlq/config/SpringTask.java
+++ src/main/java/cn/qlq/config/SpringTask.java
@@ -1,10 +1,14 @@
 package cn.qlq.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration // 通过该注解来表明该类是一个Spring的配置，相当于一个xml文件
+//开启Task
 @EnableScheduling
+//开启异步调用方法
+@EnableAsync
 public class SpringTask {
 
 }
