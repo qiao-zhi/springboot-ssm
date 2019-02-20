8d7cdfca2fc929d407308c7ee836f8d4fa431dcf
qiao_zhi <qiao_liqiang@163.com>
2019-02-20 18:39:40 +0800

    集成jsp进行开发

---
+++ src/main/java/cn/qlq/action/JSPController.java
@@ -0,0 +1,15 @@
+package cn.qlq.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("JSP")
+@Controller
+public class JSPController {
+	@RequestMapping("index")
+	public String index(ModelMap map) {
+		map.addAttribute("name", "testname");
+		return "index";
+	}
+}
