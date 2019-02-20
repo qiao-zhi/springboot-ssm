b8fb5274206dfcc6b60c8646cdbc08c3c8714e1e
qiao_zhi <qiao_liqiang@163.com>
2019-02-20 14:34:22 +0800

    1.重新整理索引 2.Mybatis改为springboot整合

--- src/main/java/cn/qlq/config/SpringConfig.java
+++ src/main/java/cn/qlq/config/SpringConfig.java
@@ -1,63 +1,19 @@
 package cn.qlq.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-import com.jolbox.bonecp.BoneCPDataSource;
+// 发布到独立的Tomcat需要继承SpringBootServletInitializer类并重写configure方法
 
-/**
- * 发布到独立的Tomcat需要继承SpringBootServletInitializer类并重写configure方法
- */
 @Configuration // 通过该注解来表明该类是一个Spring的配置，相当于一个xml文件
 @ComponentScan(basePackages = "cn.qlq") // 配置扫描包
-@PropertySource(value = { "classpath:db.properties" }, ignoreResourceNotFound = true)
-public class SpringConfig  {
-
-	@Value("${jdbc.url}")
-	private String jdbcUrl;
-
-	@Value("${jdbc.driverClassName}")
-	private String jdbcDriverClassName;
-
-	@Value("${jdbc.username}")
-	private String jdbcUsername;
-
-	@Value("${jdbc.password}")
-	private String jdbcPassword;
-
-	@Bean(destroyMethod = "close")
-	public DataSource dataSource() {
-		BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
-		// 数据库驱动
-		boneCPDataSource.setDriverClass(jdbcDriverClassName);
-		// 相应驱动的jdbcUrl
-		boneCPDataSource.setJdbcUrl(jdbcUrl);
-		// 数据库的用户名
-		boneCPDataSource.setUsername(jdbcUsername);
-		// 数据库的密码
-		boneCPDataSource.setPassword(jdbcPassword);
-		// 检查数据库连接池中空闲连接的间隔时间，单位是分，默认值：240，如果要取消则设置为0
-		boneCPDataSource.setIdleConnectionTestPeriodInMinutes(60);
-		// 连接池中未使用的链接最大存活时间，单位是分，默认值：60，如果要永远存活设置为0
-		boneCPDataSource.setIdleMaxAgeInMinutes(30);
-		// 每个分区最大的连接数
-		boneCPDataSource.setMaxConnectionsPerPartition(100);
-		// 每个分区最小的连接数
-		boneCPDataSource.setMinConnectionsPerPartition(5);
-		return boneCPDataSource;
-	}
-
-/*	@Override
+public class SpringConfig extends SpringBootServletInitializer {
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		// 设置启动类，用于独立tomcat运行的入口
 		return builder.sources(SpringConfig.class);
-	}*/
+	}
 
 }
