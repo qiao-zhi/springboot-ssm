eb733b2cf8469aee5edfea85837a4d09221b233f
qiao_zhi <qiao_liqiang@163.com>
2019-04-12 20:27:16 +0800

    整合activemq

---
+++ src/main/java/cn/qlq/activemq/Producer.java
@@ -0,0 +1,20 @@
+package cn.qlq.activemq;
+
+import javax.jms.Destination;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component("producer")
+public class Producer {
+	// 也可以注入JmsTemplate，JmsMessagingTemplate对JmsTemplate进行了封装
+	@Autowired
+	private JmsMessagingTemplate jmsTemplate;
+
+	// 发送消息，destination是发送到的队列，message是待发送的消息
+	public void sendMessage(Destination destination, final String message) {
+		jmsTemplate.convertAndSend(destination, message);
+	}
+}
