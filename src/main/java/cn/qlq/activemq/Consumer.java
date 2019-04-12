d6e97f226ef70720ed4d8cfd6f321cc5acaab9e1
qiaozhi <qiao_liqiang@163.com>
2019-04-12 23:19:08 +0800

    增加整合activeMQ

--- src/main/java/cn/qlq/activemq/Consumer.java
+++ src/main/java/cn/qlq/activemq/Consumer.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
+
 	// 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
 	@JmsListener(destination = "myQueue")
 	public void receiveQueue(String text) {
 		System.out.println("Consumer收到的报文为:" + text);
 	}
+
 }
--- src/main/java/cn/qlq/config/ListenerConfig.java
+++ src/main/java/cn/qlq/config/ListenerConfig.java
@@ -2,7 +2,9 @@ package cn.qlq.config;
 
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import cn.qlq.listener.ActiveMQListener;
 import cn.qlq.listener.MyHttpSessionListener;
 import cn.qlq.listener.MyServletContextListener;
 
@@ -11,7 +13,7 @@ import cn.qlq.listener.MyServletContextListener;
  * @author Administrator
  *
  */
-//@Configuration
+// @Configuration
 public class ListenerConfig {
 	@Bean
 	public ServletListenerRegistrationBean<MyHttpSessionListener> listenerRegist() {
@@ -24,6 +26,13 @@ public class ListenerConfig {
 	public ServletListenerRegistrationBean<MyServletContextListener> listenerRegist2() {
 		ServletListenerRegistrationBean<MyServletContextListener> srb = new ServletListenerRegistrationBean<MyServletContextListener>();
 		srb.setListener(new MyServletContextListener());
+		return srb;
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<ActiveMQListener> listenerRegist3() {
+		ServletListenerRegistrationBean<ActiveMQListener> srb = new ServletListenerRegistrationBean<ActiveMQListener>();
+		srb.setListener(new ActiveMQListener());
 		return srb;
 	}
 
