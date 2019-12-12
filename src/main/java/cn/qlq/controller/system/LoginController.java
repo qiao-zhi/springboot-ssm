4efc3023ca1240f3b23f3a547b6908d11a114ae8
qiao_zhi <qiao_liqiang@163.com>
2019-12-12 17:51:03 +0800

    bugfix:system打印日志

--- src/main/java/cn/qlq/controller/system/LoginController.java
+++ src/main/java/cn/qlq/controller/system/LoginController.java
@@ -1,7 +1,5 @@
 package cn.qlq.controller.system;
 
-import java.util.Enumeration;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
